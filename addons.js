6c3246a9adce62e2721ae65ddba3d118a9c56737
sbeuh34 <sebastien.poirier.34@gmail.com>
2021-12-14 18:56:28 +0100

    Disabled maps by default

--- addons.js
+++ addons.js
@@ -1,3 +1,3 @@
-/*global include*/
-include("js/addon-openaip.js");
-include("js/addon-franceOACI.js");
+//To enable a specific map drop just remove '//' in front of its include line
+//include("js/addon-openaip.js");
+//include("js/addon-franceOACI.js");
